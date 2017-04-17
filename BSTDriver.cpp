fb7dfb61360dfe4909c0b23f744706eabb9afec8
Jesse Robert Finley <jrfinley42@students.tntech.edu>
2017-04-17 15:18:53 -0500

    Fixed spacing in files

--- BSTDriver.cpp
+++ BSTDriver.cpp
@@ -12,72 +12,48 @@ using namespace std;
 
 void deleteCDs(ListArray<CD>* list)
 {
-   ListArrayIterator<CD>* iter = list->iterator();
+	ListArrayIterator<CD>* iter = list->iterator();
 
-   while(iter->hasNext())
-   {
-      CD* cd = iter->next();
-      delete cd;
-   }
+	while(iter->hasNext())
+	{
+		CD* cd = iter->next();
+		delete cd;
+	}
 
-   delete iter;
+	delete iter;
 }
 
 int main()
 {
-   //the unsorted ListArray of cds
-   ListArray<CD>* cds = CD::readCDs("cds.txt");
-   int numItems = cds->size();
-   cout << numItems << endl;
-   cout << endl;
+	//the unsorted ListArray of cds
+	ListArray<CD>* cds = CD::readCDs("cds.txt");
+	int numItems = cds->size();
+	cout << numItems << endl;
+	cout << endl;
 
-   //test the binary search tree
-   //insert all of the cds
-   ListArrayIterator<CD>* iter = cds->iterator();
-   BinarySearchTree<CD>* bst = new BinarySearchTree<CD>(&CD::compare_items, &CD::compare_keys);
-   while(iter->hasNext())
-   {
-      CD* cd = iter->next();
-      bst->insert(cd);
-   }
-   delete iter;
+	//test the binary search tree
+	//insert all of the cds
+	ListArrayIterator<CD>* iter = cds->iterator();
+	BinarySearchTree<CD>* bst = new BinarySearchTree<CD>(&CD::compare_items, &CD::compare_keys);
+	while(iter->hasNext())
+	{
+		CD* cd = iter->next();
+		bst->insert(cd);
+	}
+	delete iter;
 
-   BinaryTreeIterator<CD>* bst_iter = bst->iterator();
-   bst_iter->setInorder();  //takes a snapshot of the data
-   while(bst_iter->hasNext())
-   {
-      CD* cd = bst_iter->next();
-      cd->displayCD();
-   }
-   delete bst_iter;
+	BinaryTreeIterator<CD>* bst_iter = bst->iterator();
+	bst_iter->setInorder();  //takes a snapshot of the data
+	while(bst_iter->hasNext())
+	{
+		CD* cd = bst_iter->next();
+		cd->displayCD();
+	}
+	delete bst_iter;
 
-   //DO THIS
-   //display the height of the binary search tree (not minimum height)
-   //display whether the binary search tree is balanced (should not be balanced)
-
-
-
-
-
-
-
-
-   //create a minimum height binary search tree
-   BinarySearchTree<CD>* min_bst = bst->minimize();
-   bst_iter = min_bst->iterator();
-
-   //make sure that an inorder traversal gives sorted order
-   bst_iter->setInorder();  //takes a snapshot of the data
-   while(bst_iter->hasNext())
-   {
-      CD* cd = bst_iter->next();
-      cd->displayCD();
-   }
-   delete bst_iter;
-
-   //DO THIS
-   //display the height of the binary search tree (should be minimum height)
-   //display whether the binary search tree is balanced (should be balanced)
+	//DO THIS
+	//display the height of the binary search tree (not minimum height)
+	//display whether the binary search tree is balanced (should not be balanced)
 
 
 
@@ -86,24 +62,22 @@ int main()
 
 
 
+	//create a minimum height binary search tree
+	BinarySearchTree<CD>* min_bst = bst->minimize();
+	bst_iter = min_bst->iterator();
 
-   //create a complete binary search tree
-   BinarySearchTree<CD>* complete_bst = bst->minimizeComplete();
-   delete bst;
+	//make sure that an inorder traversal gives sorted order
+	bst_iter->setInorder();  //takes a snapshot of the data
+	while(bst_iter->hasNext())
+	{
+		CD* cd = bst_iter->next();
+		cd->displayCD();
+	}
+	delete bst_iter;
 
-   //make sure that an inorder traversal gives sorted order
-   bst_iter = complete_bst->iterator();
-   bst_iter->setInorder();  //takes a snapshot of the data
-   while(bst_iter->hasNext())
-   {
-      CD* cd = bst_iter->next();
-      cd->displayCD();
-   }
-   delete bst_iter;
-
-   //DO THIS
-   //display the height of the complete binary search tree (should be minimum height)
-   //display whether the binary search tree is balanced (should be balanced)
+	//DO THIS
+	//display the height of the binary search tree (should be minimum height)
+	//display whether the binary search tree is balanced (should be balanced)
 
 
 
@@ -113,9 +87,35 @@ int main()
 
 
 
-   delete complete_bst;
+	//create a complete binary search tree
+	BinarySearchTree<CD>* complete_bst = bst->minimizeComplete();
+	delete bst;
 
-   deleteCDs(cds);
-   delete cds;
-   return 0;
+	//make sure that an inorder traversal gives sorted order
+	bst_iter = complete_bst->iterator();
+	bst_iter->setInorder();  //takes a snapshot of the data
+	while(bst_iter->hasNext())
+	{
+		CD* cd = bst_iter->next();
+		cd->displayCD();
+	}
+	delete bst_iter;
+
+	//DO THIS
+	//display the height of the complete binary search tree (should be minimum height)
+	//display whether the binary search tree is balanced (should be balanced)
+
+
+
+
+
+
+
+
+
+	delete complete_bst;
+
+	deleteCDs(cds);
+	delete cds;
+	return 0;
 }
